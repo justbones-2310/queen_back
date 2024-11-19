f6972b0aae28e96d2a77a719e94560b5dc002550
Bones <justbones2310@gmail.com>
2024-11-18 23:45:53 -0300

    update

---
+++ routes/admin/administrador.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+
+router.get('/', function (req, res, next) {
+    res.render('admin/administrador', {
+        layout: 'admin/layout',
+        usuario: req.session.nombre,
+    });
+});
+
+module.exports = router;
